3598bc721848cb1ebb09b0c73a500427510b3071
Fahad Hassan <fahad.1979.hassan@gmail.com>
2025-04-14 02:17:28 +0500

    Add WindowConfig and gameloop.
    - Included `WindowConfig.h` for window settings and icon path.
    - Created a window and loaded it's icon.
    - Added new binary file `missing.png`.

---
+++ Main/WindowConfig.h
@@ -0,0 +1,16 @@
+#ifndef WINDOWCONFIG_H
+#define WINDOWCONFIG_H
+
+typedef unsigned int uint32_t;
+
+namespace window
+{
+    constexpr uint32_t
+        WIDTH = 1200,
+        HEIGHT = 600,
+        FPS = 60;
+
+    const char ICON[] = "./assets/icon.jpg";
+}
+
+#endif // WINDOWCONFIG_H
--- Main/main.cpp
+++ Main/main.cpp
@@ -1,6 +1,31 @@
-#include <SFML\Graphics.hpp>
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+#include "WindowConfig.h"
 
 int main()
 {
+    /* Window Init */
+    sf::Window window(sf::VideoMode({ window::WIDTH, window::HEIGHT }), "Plants vs Zombies");
+    window.setFramerateLimit(window::FPS);
+
+    sf::Image icon;
+    if (!icon.loadFromFile(window::ICON))
+        std::cerr << "Unable to load asset " << window::ICON << std::endl;
+    else
+        window.setIcon(icon.getSize(), icon.getPixelsPtr());
+
+    /* Game Loop */
+    while (window.isOpen())
+    {
+        /* Event Handling*/
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+    }
+
     return 0;
 }
